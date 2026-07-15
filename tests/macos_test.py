f222f48e54117c95911e17db89d9eeae2eee7672
workfromhome <popxhxh@outlook.com>
2026-07-15 16:02:43 +0800

    :bug: Fix wrong title name: it isn't VSCode on macos

--- tests/macos_test.py
+++ tests/macos_test.py
@@ -2,4 +2,4 @@ import termgif
 import os
 
 # Record a vscode window
-termgif.make_gif("", f"{os.path.dirname(__file__)}/macos_o.gif", win_name="Visual Studio Code")
+termgif.make_gif("", f"{os.path.dirname(__file__)}/macos_o.gif", win_name="termgif")

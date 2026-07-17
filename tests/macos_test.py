649b5f71d5a1cbf398d01ed0478382084c78ab82
workfromhome <popxhxh@outlook.com>
2026-07-17 11:49:36 +0800

    :test_tube: Add test for linux and macOS

--- tests/macos_test.py
+++ tests/macos_test.py
@@ -2,4 +2,6 @@ import termgif
 import os
 
 # Record a vscode window
+# The title of VSCode is different between macOS and Windows.
+# Use the name of the current folder instead.
 termgif.make_gif("", f"{os.path.dirname(__file__)}/macos_o.gif", win_name="termgif")
